9a2c496cf44e4b62a81e6b3c85dd065c97359349
prof613 <tommy@redwhiteandtruenews.com>
2025-06-29 15:22:44 -0500

    Update database.js

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -1,12 +1,15 @@
 module.exports = ({ env }) => {
-  console.log('NODE_ENV:', env('NODE_ENV'));
-  console.log('SSL config will be:', env('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false);
+  const databaseUrl = new URL(env('DATABASE_URL'));
   
   return {
     connection: {
       client: 'postgres',
       connection: {
-        connectionString: env('DATABASE_URL'),
+        host: databaseUrl.hostname,
+        port: databaseUrl.port,
+        database: databaseUrl.pathname.slice(1),
+        user: databaseUrl.username,
+        password: databaseUrl.password,
         ssl: env('NODE_ENV') === 'production' ? { rejectUnauthorized: false } : false,
       },
       debug: false,
