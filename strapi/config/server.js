35873c07159a2b8cc94af739567e805c7552e4cd
prof613 <tommy@redwhiteandtruenews.com>
2025-06-28 19:46:30 -0500

    Added dynamic URL

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -4,6 +4,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('STRAPI_URL', 'http://localhost:1337'),
+  admin: {
+    path: '/admin',
+  },
   app: {
     keys: env.array('APP_KEYS', ['default-key1', 'default-key2']),
   },
