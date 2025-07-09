5cfe15cf8eeb7fd350848e71e4e778cc42645eab
prof613 <tommy@redwhiteandtruenews.com>
2025-07-09 17:18:48 -0500

    Update middleware.js

--- strapi/config/middleware.js
+++ strapi/config/middleware.js
@@ -6,7 +6,11 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'https://redwhiteandtruenews.com'],
+      origin: [
+        'http://localhost:3000', 
+        'https://redwhiteandtruenews.com',
+        'https://rwtnews-live-frontend-s7ibg.ondigitalocean.app'
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
