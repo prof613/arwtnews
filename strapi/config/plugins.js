5ddd053fac41e559dd277f1f92006e2d0e9ba0e1
prof613 <tommy@redwhiteandtruenews.com>
2025-07-09 18:58:49 -0500

    Update plugins.js

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -11,23 +11,24 @@ module.exports = ({ env }) => ({
     enabled: true,
   },
 
-  upload: {
-    config: {
-      provider: env('UPLOAD_PROVIDER', 'local'), // Default to local
-      providerOptions: env('UPLOAD_PROVIDER') === 'aws-s3' ? {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
-        region: env('AWS_REGION'),
-        bucket: env('DO_SPACE_BUCKET'),
-        endpoint: env('DO_SPACE_ENDPOINT'),
-        s3ForcePathStyle: true,
-        params: {
-          ACL: 'public-read',
-          Bucket: env('DO_SPACE_BUCKET'),
-        },
-      } : {}, // Empty object for local provider
-    },
+upload: {
+  config: {
+    provider: env('UPLOAD_PROVIDER', 'local'), // Default to local
+    providerOptions: env('UPLOAD_PROVIDER') === 'aws-s3' ? {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+      region: env('AWS_REGION'),
+      bucket: env('DO_SPACE_BUCKET'),
+      endpoint: env('DO_SPACE_ENDPOINT'),
+      baseUrl: env('DO_SPACE_CDN'),
+      s3ForcePathStyle: true,
+      params: {
+        ACL: 'public-read',
+        Bucket: env('DO_SPACE_BUCKET'),
+      },
+    } : {}, // Empty object for local provider
   },
+},
 
   // email: {
   //   enabled: true,
