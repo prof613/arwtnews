ff9ad4d772890325ea7fbaecc29a1f7f7f23ceac
prof613 <tommy@redwhiteandtruenews.com>
2025-07-09 19:50:45 -0500

    Create middleware.js

---
+++ strapi/config/middleware.js
@@ -0,0 +1,31 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000', 
+        'https://redwhiteandtruenews.com',
+        'https://rwtnews-live-frontend-s7ibg.ondigitalocean.app'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
+  {
+  name: 'strapi::security',
+  config: {
+    contentSecurityPolicy: false,
+  },
+},
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
