5912b58e8c467f635e07a21037fb3ffc6a270e34
prof613 <tommy@redwhiteandtruenews.com>
2025-07-09 23:09:32 -0500

    Update plugins.js

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+
   'users-permissions': {
     enabled: true,
     config: {
@@ -10,25 +11,24 @@ module.exports = ({ env }) => ({
     enabled: true,
   },
 
-  // Configure DigitalOcean Spaces for file uploads
-  upload: {
-    config: {
-      provider: 'strapi-provider-upload-do',
-      providerOptions: {
-        key: env('DO_SPACE_KEY'),
-        secret: env('DO_SPACE_SECRET'),
-        endpoint: env('DO_SPACE_ENDPOINT'), // e.g., sfo3.digitaloceanspaces.com
-        space: env('DO_SPACE_BUCKET'), // e.g., your-space-name
-        cdn: env('DO_SPACE_CDN', ''), // Optional CDN URL
+upload: {
+  config: {
+    provider: env('UPLOAD_PROVIDER', 'local'), // Default to local
+    providerOptions: env('UPLOAD_PROVIDER') === 'aws-s3' ? {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_SECRET_ACCESS_KEY'),
+      region: env('AWS_REGION'),
+      bucket: env('DO_SPACE_BUCKET'),
+      endpoint: env('DO_SPACE_ENDPOINT'),
+      baseUrl: env('DO_SPACE_CDN'),
+      s3ForcePathStyle: true,
+      params: {
+        ACL: 'public-read',
+        Bucket: env('DO_SPACE_BUCKET'),
       },
-      actionOptions: {
-        upload: {
-          ACL: 'public-read',
-        },
-        delete: {},
-      },
-    },
+    } : {}, // Empty object for local provider
   },
+},
 
   // email: {
   //   enabled: true,
@@ -43,4 +43,5 @@ module.exports = ({ env }) => ({
   //     },
   //   },
   // },
+
 });
