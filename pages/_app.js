32d633239c9ff9a917bedb5f74540c91c7f8c294
prof613 <tommy@redwhiteandtruenews.com>
2025-07-18 13:35:10 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,26 @@
-import '../styles/globals.css';
-import Head from 'next/head';
+import "../styles/globals.css"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Plausible Analytics Script */}
+        <script defer data-domain="rwtnews.com" src="https://plausible.io/js/script.js"></script>
+
         <meta property="og:title" content="Red, White and True News" />
         <meta property="og:description" content="The RIGHT News for America." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.redwhiteandtruenews.com" />
-        <meta property="og:image" content="https://rwtnews-live-frontend-s7ibg.ondigitalocean.app/images/core/og-image.jpg" />
+        <meta
+          property="og:image"
+          content="https://rwtnews-live-frontend-s7ibg.ondigitalocean.app/images/core/og-image.jpg"
+        />
         <meta property="og:site_name" content="Red, White and True News" />
       </Head>
       <Component {...pageProps} />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
