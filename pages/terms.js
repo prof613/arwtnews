481368e60bc8869119b7ee2e460887c224d8099c
prof613 <tommy@redwhiteandtruenews.com>
2025-07-30 08:12:58 -0500

    og tags

--- pages/terms.js
+++ pages/terms.js
@@ -12,6 +12,17 @@ export default function Terms() {
     <>
       <Head>
         <title>Terms of Service | Red, White and True News</title>
+        <meta name="description" content="The RIGHT News For America" />
+        <meta property="og:title" content="Red, White and True News" />
+        <meta property="og:description" content="The RIGHT News For America" />
+        <meta property="og:image" content="images\core\og-image.jpg" />
+        <meta property="og:url" content="RWTNews.com" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Red, White and True News" />
+        <meta name="twitter:description" content="The RIGHT News For America" />
+        <meta name="twitter:image" content="images\core\og-image.jpg" />
+        <meta name="twitter:url" content="RWTNews.com" />
         <link rel="icon" href="/images/core/rwtn_favicon.jpg" />
       </Head>
       <Header />
