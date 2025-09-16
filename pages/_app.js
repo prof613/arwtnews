4ecc2f773647755242653bfc3038109fa312bed9
prof613 <tommy@redwhiteandtruenews.com>
2025-09-16 12:38:43 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,18 +12,13 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" /> {/* Assuming you have a favicon */}
         {/* No specific OG/Twitter tags here, they will be handled per page */}
         <meta name="facebook-domain-verification" content="w15cpa90l9h1j3wrrtbft8drm9uhvt" />
+<script type="text/javascript" src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=679cc53d924dac80554426324b337954c043e198"></script>
       </Head>
 
       {/* Plausible Analytics Script using next/script */}
       <Script
         src="https://plausible.io/js/script.js"
         data-domain="rwtnews.com"
-        strategy="afterInteractive" // Load after the page is interactive
-      />
-
-      {/* Avantlink Verification Script */}
-      <Script
-        src="http://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=679cc53d924dac80554426324b337954c043e198"
         strategy="afterInteractive" // Load after the page is interactive
       />
 
